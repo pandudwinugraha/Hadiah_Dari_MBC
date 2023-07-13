73d866183527d4bd379296f3ec8a8e32629ec61c
pandudwinugraha <139349735+pandudwinugraha@users.noreply.github.com>
2023-07-13 11:58:42 +0700

    First Create Kalkulator_Sederhana.C
    Membuat program awal dan keterangan pilihan kalkulator sederhana dalam bahasa C

---
+++ Kalkulator_Sederhana.C
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    int a, b, hasil, menu_pilihan;
+    float c, d, hasil_bagi;
+    while(1){
+    printf("===============================================\n");
+    printf("Program Kalkulator Sederhana_Hadiah Latihan MBC\n");
+    printf("===============================================\n\n");
+    printf("Menu Pilihan :\n");
+    printf("1. Penjumlahan\n2. Pengurangan\n3. Perkalian\n4. Pembagian\n");
+    printf("Pilih menu : ");
+    scanf("%d",&menu_pilihan);
+   
+     switch (menu_pilihan){
+
+
+
+      }
+    }
+return 0;
+}
